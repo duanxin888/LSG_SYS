34a5e58141a67bf5a0b73035a3916ddc2f960f6b
dx134 <18870735026@163.com>
2020-09-30 21:02:33 +0800

    add application config

--- lsg-api/src/main/java/com/duanxin/lsg/LSGServerApplication.java
+++ lsg-api/src/main/java/com/duanxin/lsg/LSGServerApplication.java
@@ -1,5 +1,6 @@
 package com.duanxin.lsg;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -10,6 +11,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  * @date 2020/09/30 14:16
  */
 @SpringBootApplication
+@MapperScan("com.duanxin.lsg.persistent.mapper")
 public class LSGServerApplication {
 
     public static void main(String[] args) {
